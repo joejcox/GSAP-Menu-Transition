b2e1db0e74bf2be5edfff986c00e55314f53ebc9
Joe Cox <joecox@live.co.uk>
2020-04-08 02:21:54 +0100

    Added menu toggle. Animation currently only on true

--- src/App.js
+++ src/App.js
@@ -1,8 +1,72 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import MainNavigation from "./components/header/MainNavigation";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuOpen: false,
+    };
+  }
+  componentDidMount() {
+    const script = document.createElement("script");
+    script.src = "https://use.fontawesome.com/releases/v5.3.1/js/all.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }
+
+  toggleMenu() {
+    this.setState({
+      menuOpen: !this.state.menuOpen,
+    });
+  }
+
+  render() {
+    console.log(this.state.menuOpen);
+    let burgerClasses;
+    let menu;
+    if (this.state.menuOpen) {
+      burgerClasses = "navbar-burger is-active";
+      menu = <MainNavigation open={true} />;
+    } else {
+      burgerClasses = "navbar-burger";
+      menu = null;
+    }
+    return (
+      <>
+        <nav
+          className="navbar-menu"
+          role="navigation"
+          aria-label="main navigation">
+          <div className="navbar-start">
+            <div className="navbar-brand">
+              <a className="navbar-item" href="/">
+                <img
+                  src="https://bulma.io/images/bulma-logo.png"
+                  alt="Bulma: Free, open source, and modern CSS framework based on Flexbox"
+                  width="112"
+                  height="28"
+                />
+              </a>
+            </div>
+          </div>
+          <div className="navbar-end">
+            <button
+              className={burgerClasses}
+              aria-label="menu"
+              aria-expanded="false"
+              onClick={() => this.toggleMenu()}>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </button>
+          </div>
+        </nav>
+        {menu}
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/header/MainNavigation.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import "./MainNavigation.scss";
+import gsap from "gsap";
+
+class MainNavigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
+
+  componentDidMount() {
+    const tl = gsap.timeline();
+    tl.fromTo(
+      "#main-navigation",
+      1,
+      { opacity: 0 },
+      { opacity: 1, ease: "Power1.easeOut" }
+    )
+      .fromTo(
+        "#main-navigation .underlay",
+        0.6,
+        { maxWidth: "0" },
+        { maxWidth: "50%", ease: "Power2.easeOut" },
+        0.4
+      )
+      .fromTo(
+        ".menu-label",
+        0.4,
+        { opacity: 0, y: 20 },
+        { opacity: 1, y: 0, ease: "Power1.easeOut" }
+      )
+      .fromTo(
+        ".menu-list li",
+        0.8,
+        { opacity: 0, y: 20 },
+        {
+          opacity: 1,
+          y: 0,
+          stagger: 0.1,
+          ease: "Power3.easeInOut",
+        },
+        1
+      );
+  }
+
+  render() {
+    return (
+      <>
+        <section className="section" id="main-navigation">
+          <div className="container">
+            <div className="columns">
+              <div className="column is-half">
+                <aside className="menu">
+                  <h2 className="menu-label title is-1">Navigation</h2>
+                  <ul className="menu-list">
+                    <li>
+                      <a href="/">About</a>
+                    </li>
+                    <li>
+                      <a href="/">Portfolio</a>
+                    </li>
+                    <li>
+                      <a href="/">Case Studies</a>
+                    </li>
+                    <li>
+                      <a href="/">Gallery</a>
+                    </li>
+                    <li>
+                      <a href="/">Contact</a>
+                    </li>
+                  </ul>
+                </aside>
+              </div>{" "}
+              {/* End Column*/}
+            </div>
+          </div>
+          <div className="underlay"></div>
+        </section>
+      </>
+    );
+  }
+}
+
+export default MainNavigation;
